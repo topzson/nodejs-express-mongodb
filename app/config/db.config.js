018623197a39536a0fa27518d159866ae9ce4f78
Rattatammanoon Prawasuk <toprattatammanoon@hotmail.com>
2023-05-29 15:26:44 +0700

    set env connect mongodb

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,4 +1,5 @@
+require('dotenv').config()
 module.exports = {
-    //url: "mongodb+srv://Topzson:<password>@cluster0.8zqysz0.mongodb.net/"
-    url: "mongodb://localhost:27017/topzson_db"
+    url: "mongodb+srv://Topzson:"+process.env.ENV_PASSWORD+"@cluster0.8zqysz0.mongodb.net"
+    // url: "mongodb://localhost:27017/topzson_db"
   };
